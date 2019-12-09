8affb5d7b5e7b5027a8ce0cdbce1d00836919ce2
rkaren <karen20mm@gmail.com>
2019-12-09 18:27:58 +0100

    pregunta1

--- pregunta1/pregunta1.sh
+++ pregunta1/pregunta1.sh
@@ -6,21 +6,15 @@
 
 #variables globales
 let adaptador
+let direccion
+let puerta
+let mascara
+let broadcast
+let dns1
+let dns2
 
 #FUNCIONES
-#FUNCION SUMA
-function_suma(){
-    rm /tmp/calc.tmp
-    rm /tmp/calc2.tmp
-    # Dibuja funcion suma
-    dialog --inputbox "Indica el primer numero" 0 0 2>$OUTPUT
-    # lee lo que  escribe Y ASIGNA A 
-    n1=$(<$OUTPUT)
-    dialog --inputbox "Indica el segundo numero" 0 0 2>$OUTPUT2
-    n2=$(<$OUTPUT2)
-    result=$((n1+n2))
-    dialog --msgbox  "result: "$result 0 0
-}
+
 #funcion para ingresar adapatador de red
 function_adaptador(){
     OUTPUT="/tmp/input.txt"
@@ -39,6 +33,90 @@ function_adaptador(){
     #lo asignamos a variable global
     adaptador=$name
 }
+
+#funcion para ingresar direccion ip
+function_direccion(){
+   #dialog --inputbox "Indica el direccion IP" 0 0 2>$OUTPUT
+    # lee lo que  escribe Y ASIGNA A 
+  #  n2=$(<$OUTPUT)
+  #  dialog --msgbox  "ingresaste: "$n2 0 0
+    OUTPUT="/tmp/input.txt"
+    dialog --inputbox "Indica el direccion" 0 0 2>$OUTPUT
+ # lee lo que está escribiendo el usuario
+    respose=$?
+    # lee lo que hay en OUTPUT  y lo redirige
+    name=$(<$OUTPUT)
+    # Entra en el case
+    case $respose in
+      0) echo "hola $name" ;;
+      1) echo "Cancel pressed." ;;
+      255) echo "[ESC] key pressed."
+    esac
+    rm /tmp/input.txt
+    #lo asignamos a variable global
+    direccion=$name
+}
+#Funcion para ingresar
+function_puerta(){
+  OUTPUT="/tmp/input.txt"
+    dialog --inputbox "Indica el puerta" 0 0 2>$OUTPUT
+ # lee lo que está escribiendo el usuario
+    respose=$?
+    # lee lo que hay en OUTPUT  y lo redirige
+    name=$(<$OUTPUT)
+    # Entra en el case
+    case $respose in
+      0) echo "hola $name" ;;
+      1) echo "Cancel pressed." ;;
+      255) echo "[ESC] key pressed."
+    esac
+    rm /tmp/input.txt
+    #lo asignamos a variable global
+   puerta=$name
+}
+#funcion para ingresar mascara
+function_mascara(){
+OUTPUT="/tmp/input.txt"
+    dialog --inputbox "Indica el mascara" 0 0 2>$OUTPUT
+ # lee lo que está escribiendo el usuario
+    respose=$?
+    # lee lo que hay en OUTPUT  y lo redirige
+    name=$(<$OUTPUT)
+    # Entra en el case
+    case $respose in
+      0) echo "hola $name" ;;
+      1) echo "Cancel pressed." ;;
+      255) echo "[ESC] key pressed."
+    esac
+    rm /tmp/input.txt
+    #lo asignamos a variable global
+    mascara=$name
+}
+#funcion para ingresar el broadcast
+function_broadcast(){
+  OUTPUT="/tmp/input.txt"
+    dialog --inputbox "Indica el broadcast" 0 0 2>$OUTPUT
+ # lee lo que está escribiendo el usuario
+    respose=$?
+    # lee lo que hay en OUTPUT  y lo redirige
+    name=$(<$OUTPUT)
+    # Entra en el case
+    case $respose in
+      0) echo "hola $name" ;;
+      1) echo "Cancel pressed." ;;
+      255) echo "[ESC] key pressed."
+    esac
+    rm /tmp/input.txt
+    #lo asignamos a variable global
+   broadcast=$name
+}
+#funcion para ingresar el dns1
+#fucntion_dns1(){
+#}
+#funcion para ingresar el dns2
+#function_dns2(){
+#}
+
 function_crear(){
    # variuablke q almacaena cada fila del formato de interfcaes
     let fila
@@ -46,42 +124,23 @@ function_crear(){
     #cat $adaptador #cat direccion cat puerta cat mascara cat broadcast cat dns1 cat dns2
     # empezamos el dibujo del a estructura del fichero inetrfaces
     fila= "source /etc/network/interfaces.d/*"   
-    echo $fila >>interfaces.txt
+    echo $fila >>interfaces
     fila="auto $adaptador"
     #lo redireccionamos interfaces .txt(no es .txt en realidad solo para este examen)
-    echo $fila >>interfaces.txt
+    echo $fila >>interfaces
     fila="iface $adaptador inet static"
-    echo $fila >>interfaces.txt
+    echo $fila >>interfaces
+    fila="Address $direccion"
+    echo $fila >>interfaces
+    fila="Netmask $mascara"
+    echo $fila >>interfaces
+    fila="Broadcast $broadcast"
+    echo $fila >>interfaces
 
 }
-#funcion para ingresar direccion ip
-#function_direccion(){
- #   dialog --inputbox "Indica el direccion IP" 0 0 2>$OUTPUT
-    # lee lo que  escribe Y ASIGNA A 
-  #  n2=$(<$OUTPUT)
-  #  dialog --msgbox  "ingresaste: "$n2 0 0
-#}
-#Funcion para ingresar
-#function_puerta(){
-#}
-#funcion para ingresar mascara
-#function_mascara(){
-#}
-#funcion para ingresar el broadcast
-#function_broadcast(){
-#}
-#funcion para ingresar el dns1
-#fucntion_dns1(){
-#}
-#funcion para ingresar el dns2
-#function_dns2(){
-#}
 #funcion para mostrar los datos
 function_datos(){
-   dialog --title "Indica el primer numero" \
-	--backtitle "bakctitle" \
- #  dialog --msgbox $OUTPUT
-	dialog --msgbox adaptador
+  cat interfaces
 }
 #Menu principal
 function_menu(){
@@ -96,7 +155,7 @@ function_menu(){
                 Direccion_de_broadcast "Broadcast" \
 		DNS_1 "dns1" \
 		DNS_2 "dns2" \
-		Mostrar "datos" \
+		#Mostrar "datos" \
 		Crear "crear" \
                 Salir "Salir del Script" 2>"${INPUT}"
 
@@ -113,7 +172,7 @@ function_menu(){
                 Dns_1) echo "Elegiste dns1"; function_dns1;;
                 Dns_2) echo "Elegiste dns2"; function_dns2;;
                 Crear) echo "Elegiste crear"; function_crear;;
-                Mostrar) echo "Elegiste mostrar"; function_datos;;
+               # Mostrar) echo "Elegiste mostrar"; function_datos;;
                 Exit) echo "Bye";
         esac
 }
